a470621001b2e83b3566586dea178cb09f58fc4c
nicatabdullay3v <nicat.neon@bk.ru>
2023-12-01 17:54:10 +0400

    image

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="nav_right">
+    <div className="nav_logo">
+        <img src="" alt="" />
+    </div>
+        </div>
+        <div className="nav_center">
+
+        </div>
+        <div className="nav_left">
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
+
