8d36d23bc57485f6b4655660259affcb12a89143
unknown <bd7fcqzu3@code.edu.az>
2023-12-06 22:38:07 +0400

    sn

--- src/components/admin/pages/parents/CreatePage/CreatePage.jsx
+++ src/components/admin/pages/parents/CreatePage/CreatePage.jsx
@@ -9,9 +9,7 @@ YupPassword(Yup);
 
 import { fetcBabysitterJobs } from "../../../../../redux/Slice/BabySittersSlice/BabySittersSlice";
 
-const SignupSchema = Yup.object().shape({
-
-});
+const SignupSchema = Yup.object().shape({});
 
 const CreatePage = ({ setcreatePage }) => {
   const dispatch = useDispatch();
@@ -96,11 +94,7 @@ const CreatePage = ({ setcreatePage }) => {
         .min(100, "Must be 100 characters or more")
 
         .required("Required"),
-<<<<<<< HEAD
-      // numberofchildren: Yup.number().required("Required"),
-=======
-      // numberofchildren: Yup.string().required("Required"),
->>>>>>> bb5e5a466f39d87de7c50723cc15cb2b9070b5ba
+
       childrenAge: Yup.string().required("Required"),
       password: Yup.string()
         .min(
@@ -140,7 +134,6 @@ const CreatePage = ({ setcreatePage }) => {
           <div className="right_side_create">
             <div className="create_to">
               <div className="card_parent">
-                
                 <form onSubmit={formik.handleSubmit} action="">
                   <div className="firstname_input">
                     <label htmlFor="firstName">firstName:</label>
