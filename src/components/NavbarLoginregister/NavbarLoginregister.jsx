1cb5d581f8ecb8cc098410fed1ea00e247a498ce
nicatabdullay3v <nicat.neon@bk.ru>
2023-12-07 17:26:31 +0400

    NavBars100%

--- src/components/NavbarLoginregister/NavbarLoginregister.jsx
+++ src/components/NavbarLoginregister/NavbarLoginregister.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Navbar.scss";
+import "./NavbarLoginregister.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { Link, Outlet, useNavigate } from "react-router-dom";
@@ -23,29 +23,7 @@ const Navbar = () => {
             <img src="./../../../src/assets/images/logo.main.png" alt="" />
           </div>
         </div>
-        <div className="nav_center">
-          <div className="nav_center_text">
-            <Link to="/BabySitters" className="link">
-              Babysitters
-            </Link>
-          </div>
-          <div className="nav_center_text">
-            <Link to="/BabySittingJobs" className="link">
-              Babysitting jobs
-            </Link>
-          </div>
-          <div className="nav_center_text">
-            <Link className="link" to="/Howitworks">
-              {" "}
-              How it works
-            </Link>
-          </div>
-          <div className="nav_center_text">
-            <Link className="link" to="/Pricing">
-              Pricing
-            </Link>
-          </div>
-        </div>
+     
         <div className="nav_right">
         <div className="nav_right_button_login">
             <Link to="/Login" className="link">
